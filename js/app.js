a1a66f413523c9b618c25315a76e852bd0ab3c6b
mnqobiMenzi <66906535+mnqobiMenzi@users.noreply.github.com>
2020-08-26 11:16:08 +0200

    top button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 /*Navigation*/
-$(document).ready(function(){
-    $('#icon').click(function(){
+$(document).ready(function() {
+    $('#icon').click(function() {
         $('ul').toggleClass('show');
     });
 });
+
+// top button
+var myBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+        myBtn.style.display = "block";
+    } else {
+        myBtn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
